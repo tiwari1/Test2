c9dc669033165d39457695f03e28c31a89a1e4b9
Akshita Tiwari <85664297+tiwari1@users.noreply.github.com>
2024-03-31 23:32:01 +0530

    Add files via upload

---
+++ Test 2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#slicing
+x='interview'
+print(x[1:6]+x[7:])
+
+
+# In[2]:
+
+
+#slicing
+x='initial interest'
+print(x[1:2]+x[3:4]+x[5:8]+x[9:])
+
+
+# In[10]:
+
+
+#wap to accept the city from the user and its res. monument
+x=input("enter the city name: ")
+if x=='delhi':
+    print("Red Fort")
+elif x=='agra':
+    print("taj mahal")
+elif x=='jaipur':
+    print("Jal Mahal")
+else:
+    print("Invalid")
+
+
+# In[7]:
+
+
+#wap following if a=9
+a=int(input("enter the required number: "))
+if(a>5 and a<=10):
+    print("Hello")
+else:
+    print("Bye")
+
+
+# In[9]:
+
+
+#write a logical expression for the following:
+# A is greater than B and C is greater than D
+A=int(input("enter the required number: "))
+B=int(input("enter the required number: "))
+C=int(input("enter the required number: "))
+D=int(input("enter the required number: "))
+if (A > B and C > D):
+    print("hii")
+else:
+    print("invalid")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
